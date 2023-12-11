7e8efa8ace7278700d724993ad67833d1763a555
minchonang <green8085@naver.com>
2023-12-11 23:11:33 +0900

    시각화 html, 데이터프레임 보는 버튼, 영역 추가

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let bool_dataFrameState = {
+      df1: false,
+      df2: false
+}
+
+
+
 const TAB_NONE = "tab_none";
 const TAB_SELECTED = "tab_selected";
 const HIDDEN = "hidden";
@@ -8,6 +15,11 @@ const env = document.querySelector("#tab_env");
 const ecoCar_graph = document.querySelector("#ecoCar_graph");
 const env_graph = document.querySelector("#env_graph");
 const comment = document.querySelector("#comment");
+const df = {
+      df1 : document.querySelector("#dataFrame1"),
+      df2 : document.querySelector("#dataFrame2")
+};
+
 
 const selectEcoCar = () => {
       ecoCar.classList.replace(TAB_NONE, TAB_SELECTED);
@@ -15,7 +27,7 @@ const selectEcoCar = () => {
       ecoCar_graph.classList.replace(HIDDEN, SHOW);
       env_graph.classList.replace(SHOW, HIDDEN);
       comment.classList.replace(HIDDEN, "comment_area");
-}
+};
 
 const selectEnv = () => {
       env.classList.replace(TAB_NONE, TAB_SELECTED);
@@ -23,4 +35,16 @@ const selectEnv = () => {
       env_graph.classList.replace(HIDDEN, SHOW);
       ecoCar_graph.classList.replace(SHOW, HIDDEN);
       comment.classList.replace("comment_area", HIDDEN);
-}
+};
+
+const click_DataFrame = (key) => {
+      console.log(df[key])
+      if (!bool_dataFrameState[key]) {
+        df[key].classList.replace(HIDDEN, "df");
+        bool_dataFrameState[key] = true;
+      } else if (bool_dataFrameState[key]) {
+        df[key].classList.replace("df", HIDDEN);
+        bool_dataFrameState[key] = false;
+      }
+};
+
